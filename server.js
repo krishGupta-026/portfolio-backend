73bc7febcc3c01f4044044c44d3e2edc72907805
krishGupta <74751117+krishGupta-026@users.noreply.github.com>
2026-01-15 23:56:55 +0530

    Update email transport settings and fix port

--- server.js
+++ server.js
@@ -42,19 +42,16 @@ app.post("/api/contact", async (req, res) => {
 
     // ✅ EMAIL TRANSPORTER (IMPORTANT)
     const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+        },
+      connectionTimeout: 20000,
+      });
 
-  // ✅ timeouts (fix for Render timeout)
-  connectionTimeout: 20000,
-  greetingTimeout: 20000,
-  socketTimeout: 20000,
-});
 
 
     // ✅ Mail Content
@@ -98,4 +95,5 @@ ${message}
 // ✅ Start Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+
 
